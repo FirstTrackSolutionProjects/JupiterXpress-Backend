1d81256bc064b1f65e94fef99cdce08833b3d208
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-09-24 22:28:55 +0530

    chore : added submitKYC route

--- controllers/merchantController.js
+++ controllers/merchantController.js
@@ -30,11 +30,11 @@ const createIncompleteVerifyRequest = async (req, res) => {
                 account,
                 cin
             } = req.body;
-            if (!address || !state || !city || !pin || !aadhar || !pan || !gst || !msme || !bank || !ifsc || !account || !cin){
+            if (!address || !state || !city || !pin || !aadhar || !pan || !gst || !msme || !bank || !ifsc || !account || !cin) {
                 return res.status(400).json({
                     status: 400, message: 'All fields are required'
                 });
-            } 
+            }
             try {
                 const [requests] = await db.query('SELECT * FROM MERCHANT_VERIFICATION WHERE uid = ? AND status = "pending"', [id]);
                 if (requests.length) {
@@ -132,4 +132,56 @@ const activateMerchant = async (req, res) => {
     }
 }
 
-module.exports = { createIncompleteVerifyRequest, activateMerchant }
+
+const submitKYC = async (req, res) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({
+            status: 401,
+            message: "Access Denied",
+        });
+    }
+
+    try {
+        const verified = jwt.verify(token, SECRET_KEY);
+        const id = verified.id;
+        try {
+            const [req] = await db.query(
+                "SELECT * FROM KYC_UPDATE_REQUEST WHERE status='INCOMPLETE' AND uid = ?",
+                [id]
+            );
+            if (req.length > 0) {
+                await db.query(
+                    "UPDATE KYC_UPDATE_REQUEST set status='PENDING' WHERE status='INCOMPLETE' AND uid = ?",
+                    [id]
+                );
+                return res.status(200).json({
+                    status: 200,
+                    success: true,
+                    message: "KYC Request Submitted Successfully",
+                });
+            } else {
+                return res.status(400).json({
+                    status: 400,
+                    success: true,
+                    message: "You already have a pending KYC Request",
+                });
+            }
+        } catch (error) {
+            return res.status(500).json({
+                status: 500,
+                message: error.message,
+                error: error.message,
+            });
+        }
+
+    } catch (err) {
+        return res.status(400).json({
+            status: 400,
+            message: "Invalid Token",
+        });
+    }
+}
+
+
+module.exports = { createIncompleteVerifyRequest, activateMerchant, submitKYC }
--- routes/merchantRoutes.js
+++ routes/merchantRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { createIncompleteVerifyRequest, activateMerchant } = require('../controllers/merchantController');
+const { createIncompleteVerifyRequest, activateMerchant, submitKYC } = require('../controllers/merchantController');
 
 const router = express.Router();
 
 router.post('/verification/createIncompleteVerifyRequest', createIncompleteVerifyRequest);
 
+
+router.post('/kyc/submit', submitKYC);
 
 
 router.post('/activate', activateMerchant);
