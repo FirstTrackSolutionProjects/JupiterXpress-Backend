2f32fa7cdc2a8d86452074612500332d099838d9
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-09-27 16:29:41 +0530

    chore : added /email/verification/verify route

--- routes/emailVerificationRoutes.js
+++ routes/emailVerificationRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { requestEmailVerificationOTP } = require('../controllers/emailVerificationController');
+const { requestEmailVerificationOTP, verifyEmail } = require('../controllers/emailVerificationController');
 
 const router = express.Router();
 
 router.post('/request/otp', requestEmailVerificationOTP);
+router.post('/verify', verifyEmail)
 
 module.exports = router;
