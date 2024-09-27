7622d2cd7c6fb46ca0b20ec78ce2cd6bd8434478
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-09-27 16:39:35 +0530

    chore : added acceptKYCRequest function

--- controllers/kycController.js
+++ controllers/kycController.js
@@ -303,6 +303,58 @@ const rejectKYCRequest = async (req, res) => {
     }
 }
 
+const acceptKYCRequest = async (req, res) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({
+            status: 401, message: 'Access Denied'
+        });
+    }
+    const { uid, kycId } = req.body;
+    if (!uid || !kycId) {
+        return res.status(400).json({
+            status: 400, message: 'Missing uid or kycId'
+        });
+    }
+    try {
+        const verified = jwt.verify(token, SECRET_KEY);
+        const admin = verified.admin;
+        const id = verified.id;
+        if (!admin) {
+            return res.status(400).json({
+                status: 400, message: 'Not an admin'
+            });
+        }
+        try {
+
+            const [requests] = await db.query("SELECT * FROM KYC_UPDATE_REQUEST WHERE kycId = ? ", [kycId]);
+            const userData = requests[0];
+            await db.query("UPDATE USER_DATA SET address = ?, state = ?, city =?, pin =?, aadhar_number=?, pan_number=?, gst=?, cin =?,msme=?, accountNumber=?, ifsc=?, bank=?, aadhar_doc=?, pan_doc=?, selfie_doc=?, gst_doc=?, cancelledCheque=? WHERE uid = ? ", [userData.address, userData.state, userData.city, userData.pin, userData.aadhar_number, userData.pan_number, userData.gst, userData.cin, userData.msme, userData.accountNumber, userData.ifsc, userData.bank, userData.aadharDoc, userData.panDoc, userData.selfieDoc, userData.gstDoc, userData.cancelledCheque, uid])
+            await db.query("UPDATE KYC_UPDATE_REQUEST SET status='ACCEPTED', actionBy=? WHERE kycId = ? ", [id, kycId]);
+            const [users] = await db.query("SELECT * FROM USERS WHERE uid = ?", [uid]);
+            const { email, fullName } = users[0];
+            let mailOptions = {
+                from: process.env.EMAIL_USER,
+                to: email,
+                subject: 'KYC has been verified',
+                text: `Dear ${fullName}, \nYour account KYC has been verified and your profile is updated according to it. Login now and take the experience of our minimal documentation domestic and international shipments`
+            };
+            await transporter.sendMail(mailOptions);
+            return res.status(200).json({
+                status: 200, success: true, message: 'KYC successfully verified'
+            })
+        } catch (error) {
+            return res.status(500).json({
+                status: 500, message: error.message, error: error.message
+            })
+        }
+    } catch (err) {
+        return res.status(400).json({
+            status: 400, message: 'Invalid Token'
+        });
+    }
+}
+
 module.exports = {
     getKYCDocumentStatus,
     submitKYC,
@@ -310,5 +362,6 @@ module.exports = {
     getAllPendingKYCRequests,
     submitIncompleteKYC,
     rejectKYCRequest,
-    updateKYCDocumentStatus
+    updateKYCDocumentStatus,
+    acceptKYCRequest
 }
