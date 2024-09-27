ca2a507607e624a5c73c990683613fde28ddd17a
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-09-27 16:28:03 +0530

    chore : added verifyEmail function

--- controllers/emailVerificationController.js
+++ controllers/emailVerificationController.js
@@ -41,6 +41,44 @@ const requestEmailVerificationOTP = async (req, res) => {
     }
 }
 
+const verifyEmail = async (req, res) => {
+    try {
+        const { email, otp } = req.body;
+        if (!email || !otp) {
+            return res.status(400).json({ message: 'Email and OTP are required' });
+        }
+
+        const [users] = await db.query('SELECT * FROM USERS WHERE email = ?', [email]);
+        if (!users.length) {
+            return res.status(400).json({ message: 'User not found' })
+        }
+        const inOtp = users[0].secret;
+        if (inOtp == otp) {
+            const transaction = await db.beginTransaction();
+            await transaction.query('UPDATE USERS SET emailVerified = ? WHERE email = ?', [1, email])
+            await transaction.query('UPDATE USERS SET secret = ? WHERE email = ?', [null, email])
+            await db.commit(transaction);
+        }
+        const { fullName } = users[0];
+        let mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: 'Email Verified Successfully',
+            text: `Dear ${fullName}, \nYour account email has been verified.\nRegards,\nJupiter Xpress`
+        };
+        await transporter.sendMail(mailOptions);
+        return res.status(200).json({
+            status: 200, success: true, message: 'Email Verified'
+        });
+
+    } catch (err) {
+        return res.status(500).json({
+            status: 500, message: 'Something went wrong'
+        })
+    }
+}
+
 module.exports = {
-    requestEmailVerificationOTP
+    requestEmailVerificationOTP,
+    verifyEmail
 }
