e27e487f4ffb92264424202f30c2fe7d3a5d9ef0
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-09-27 16:13:54 +0530

    chore : added /kyc/documentStatus/update route

--- routes/kycRoutes.js
+++ routes/kycRoutes.js
@@ -6,13 +6,15 @@ const {
     getIncompleteKYC,
     getAllPendingKYCRequests,
     submitIncompleteKYC,
-    rejectKYCRequest
+    rejectKYCRequest,
+    updateKYCDocumentStatus
  } = require('../controllers/kycController');
 
 const router = express.Router();
 
 router.post('/submit', submitKYC);
 router.post('/documentStatus', getKYCDocumentStatus)
+router.post('/documentStatus/update', updateKYCDocumentStatus)
 router.post('/incomplete', getIncompleteKYC);
 router.post('/pending/all', getAllPendingKYCRequests)
 router.post('/submit/incomplete', submitIncompleteKYC);
