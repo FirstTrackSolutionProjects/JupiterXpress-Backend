8bf452053f706fbef1904dec41d1d230d4683644
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-09-27 16:58:10 +0530

    chore : added /warehouse/update route

--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getAllWarehouses, getWarehouses, createWarehouse } = require('../controllers/warehouseController');
+const { getAllWarehouses, getWarehouses, createWarehouse, updateWarehouse } = require('../controllers/warehouseController');
 
 const router = express.Router();
 
 router.post('/warehouses/all', getAllWarehouses);
 router.post('/warehouses', getWarehouses);
 router.post('/create', createWarehouse);
+router.post('/update', updateWarehouse);
 
 module.exports = router;
