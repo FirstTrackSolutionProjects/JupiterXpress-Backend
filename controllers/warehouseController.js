bdde19563582aa2539cdcca1bdfe45b64cd9bb39
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-09-27 12:33:40 +0530

    chore : added getWarehouses route

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -26,4 +26,26 @@ const getAllWarehouses = async (req, res) => {
     }
 }
 
-module.exports = {getAllWarehouses};
+const getWarehouses = async (req, res) => {
+    const token = req.headers.authorization;
+    const verified = jwt.verify(token, SECRET_KEY);
+    const id = verified.id;
+    if (!id) {
+        return res.status(400).json({
+            status: 400, message: 'Access Denied'
+        });
+    }
+    try {
+        const [rows] = await db.query('SELECT * FROM WAREHOUSES WHERE uid = ?', [id]);
+        return res.status(200).json({
+            status: 200, rows
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            status: 500, message: error.message, success: false
+        });
+    }
+}
+
+module.exports = { getAllWarehouses, getWarehouses };
--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getAllWarehouses } = require('../controllers/warehouseController');
+const { getAllWarehouses, getWarehouses } = require('../controllers/warehouseController');
 
 const router = express.Router();
 
 router.post('/warehouses/all', getAllWarehouses);
+router.post('/warehouses', getWarehouses);
 
 module.exports = router;
