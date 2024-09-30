1c41c196ce34b5957eeaa3ccd8800c8808fdb1ad
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-09-30 08:44:15 +0530

    fix : cors was not enabled

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const serverless = require('serverless-http');
 require('dotenv').config()
 const {connectDB} = require('./utils/db');
@@ -22,6 +23,7 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use('/test', testRoute);
 app.use('/auth', authRoutes);
 app.use('/password', passwordRoutes);
