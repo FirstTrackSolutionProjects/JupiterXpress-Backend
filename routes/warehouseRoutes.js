a9fe2ccb75d6e7cd40f9ea839e1681bcf917dba6
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-10-08 18:11:44 +0530

    chore : added route reattempt feature for warehouse creation

--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllWarehouses, getWarehouses, createWarehouse, updateWarehouse } = require('../controllers/warehouseController');
+const { getAllWarehouses, getWarehouses, createWarehouse, updateWarehouse, reAttemptWarehouseCreation } = require('../controllers/warehouseController');
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.post('/warehouses/all', getAllWarehouses);
 router.post('/warehouses', getWarehouses);
 router.post('/create', createWarehouse);
 router.post('/update', updateWarehouse);
+router.post('/create/retry', reAttemptWarehouseCreation);
 
 module.exports = router;
