c3f3c045b0dddf67f23a3a2b2c549ad57c8e49c2
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-10-22 23:48:43 +0530

    feat : added feature to get relevant information from db to include in excel sheet at frontend

--- controllers/shipmentController.js
+++ controllers/shipmentController.js
@@ -1595,6 +1595,77 @@ const updateDomesticProcessingShipments = async (req, res) => {
         });
     }
 }
+
+const getAllDomesticShipmentReportsData = async (req, res) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({
+            status: 401,
+            message: 'Access Denied'
+        });
+    }
+    try {
+        const verified = jwt.verify(token, SECRET_KEY);
+        const admin = verified.admin;
+        if (!admin) {
+            return res.status(401).json({
+                status: 401,
+                message: 'Unauthorized!'
+            });
+        }
+        const { startDate, endDate } = req.body;
+        if (!startDate ||!endDate) {
+            return res.status(400).json({
+                status: 400,
+                message: 'Start date and end date are required'
+            });
+        }
+        const [reportData] = await db.query(`SELECT 
+                                            s.ord_id AS ORDER_ID,
+                                            u.uid AS MERCHANT_ID,
+                                            u.businessName AS MERCHANT_BUSINESS_NAME, 
+                                            u.email AS MERCHANT_EMAIL, 
+                                            u.phone AS MERCHANT_PHONE_NUMBER, 
+                                            w.warehouseName AS SHIPPER_WAREHOUSE_NAME, 
+                                            w.address AS SHIPPER_WAREHOUSE_ADDRESS, 
+                                            w.pin AS SHIPPER_WAREHOUSE_PIN, 
+                                            w.city AS SHIPPER_WAREHOUSE_CITY, 
+                                            w.state AS SHIPPER_WAREHOUSE_STATE, 
+                                            w.country AS SHIPPER_WAREHOUSE_COUNTRY,
+                                            s.pay_method AS SHIPMENT_PAYMENT_METHOD,
+                                            s.customer_name AS CUSTOMER_NAME,
+                                            s.customer_mobile AS CUSTOMER_CONTACT, 
+                                            s.customer_email AS CUSTOMER_EMAIL, 
+                                            s.shipping_address AS SHIPPING_ADDRESS, 
+                                            s.shipping_postcode AS SHIPPING_PINCODE,
+                                            s.shipping_city AS SHIPPING_CITY,
+                                            s.shipping_state AS SHIPPING_STATE, 
+                                            s.shipping_country AS SHIPPING_COUNTRY,
+                                            s.same AS SHIPPING_IS_BILLING,
+                                            s.billing_address AS BILLING_ADDRESS,
+                                            s.billing_postcode AS BILLING_PINCODE, 
+                                            s.billing_city AS BILLING_CITY, 
+                                            s.billing_state AS BILLING_STATE, 
+                                            s.billing_country AS BILLING_COUNTRY,
+                                            s.awb AS AWB,
+                                            s.ewaybill AS EWAYBILL
+                                            FROM SHIPMENTS s
+                                            JOIN USERS u ON u.uid = s.uid 
+                                            JOIN WAREHOUSES w ON w.wid = s.wid 
+                                            WHERE
+                                            is_manifested = true AND 
+                                            date BETWEEN ? AND ?`, [startDate+'T00:00:00',endDate+'T23:59:59'])
+        return res.status(200).json({ status: 200, data: reportData, success: true });
+    }
+    catch (err){
+        console.error(err)
+        return res.status(500).json({
+            status: 500,
+            message: 'Internal Server Error'
+        });
+    }
+}
+
 module.exports = {
     cancelShipment,
     createDomesticShipment,
@@ -1609,6 +1680,7 @@ module.exports = {
     internationalShipmentPricingInquiry,
     domesticShipmentPickupSchedule,
     trackShipment,
-    updateDomesticProcessingShipments
+    updateDomesticProcessingShipments,
+    getAllDomesticShipmentReportsData
 };
 
