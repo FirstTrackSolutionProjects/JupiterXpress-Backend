f5b2685e2926bbd30d19e4c2834e8366fdeca7f0
AdityaKumar5155 <adityakumar5155@gmail.com>
2025-08-24 13:55:45 +0530

    feat: updated tranasactions

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -1,32 +1,36 @@
 const express = require('express');
 const { createRazorpayOrderId, 
-        getAllRechargeTransactions, 
+        // getAllRechargeTransactions, 
         getBalance,
-        getAllExpenseTransactions ,
-        getAllManualRechargeTransactions,
-        getAllRefundTransactions,
+        // getAllExpenseTransactions ,
+        // getAllManualRechargeTransactions,
+        // getAllRefundTransactions,
         manualRecharge,
         verifyRazorpayRecharge,
-        getAllDisputeChargesTransactions,
+        // getAllDisputeChargesTransactions,
         exportAllTransactionsJSON,
         exportTransactionsJSON,
-        getTransactions
+        // getTransactions,
+        getAllWalletTransactionsAdmin,
+        getAllWalletTransactionsUser
     } = require('../controllers/walletController');
+const {authMiddleware} = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.post('/razorpay/createOrderId', createRazorpayOrderId);
-router.post('/recharges', getAllRechargeTransactions);
-router.post('/expenses', getAllExpenseTransactions);
-router.post('/manualRecharges', getAllManualRechargeTransactions);
-router.post('/refunds', getAllRefundTransactions)
+// router.post('/recharges', getAllRechargeTransactions);
+// router.post('/expenses', getAllExpenseTransactions);
+// router.post('/manualRecharges', getAllManualRechargeTransactions);
+// router.post('/refunds', getAllRefundTransactions)
 router.post('/balance', getBalance)
 router.post('/manualRecharge', manualRecharge)
 router.post('/verify/recharge', verifyRazorpayRecharge)
-router.get('/dispute-charges', getAllDisputeChargesTransactions)
+// router.get('/dispute-charges', getAllDisputeChargesTransactions)
 router.post('/report/download/all', exportAllTransactionsJSON)
 router.post('/report/download', exportTransactionsJSON)
-router.get('/transactions', getTransactions)
+router.get('/transactions/admin', authMiddleware ,getAllWalletTransactionsAdmin);
+router.get('/transactions/merchant', authMiddleware ,getAllWalletTransactionsUser);
 
 
 
